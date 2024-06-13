36e9b7b929d100769101f44d706dc0e095b8a602
andrepinotti <andrepinotti0@gmail.com>
2024-06-13 18:50:19 -0300

    feat: prat2 e prat3

--- Aula7-28.03/Praticando2/index.php
+++ Aula7-28.03/Praticando2/index.php
@@ -1,141 +1,95 @@
-<?php
+<?php 
 
-    
-         
+$nome = filter_input(INPUT_GET, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+$email = filter_input(INPUT_GET, 'email', FILTER_SANITIZE_EMAIL);
+$cor = filter_input(INPUT_GET, 'cor', FILTER_SANITIZE_SPECIAL_CHARS);
 
-    $nome = filter_input(INPUT_GET, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
-    $email = filter_input(INPUT_GET, 'email', FILTER_SANITIZE_EMAIL);
-    $cor = filter_input(INPUT_GET, 'cor', FILTER_SANITIZE_SPECIAL_CHARS);
-    
-    if (empty($nome) && empty($email) && empty($cor)) {
-      $nome = $_COOKIE['nome'] ?? '';
-      $email = $_COOKIE['email'] ?? '';
-      $cor = $_COOKIE['cor'] ?? '';
-      setcookie('nome', $nome, time() + 3600);
-      setcookie('email', $email, time() + 3600);
-      setcookie('cor', $cor, time() + 3600);
-    } else {
-      setcookie('nome', $nome, time() + 3600);
-      setcookie('email', $email, time() + 3600);
-      setcookie('cor', $cor, time() + 3600);
-    }
-    
-
-
-
+if (empty($nome) && empty($email) && empty($cor)) {
+  $nome = $_COOKIE['nome'] ?? '';
+  $email = $_COOKIE['email'] ?? '';
+  $cor = $_COOKIE['cor'] ?? '';
+  setcookie('nome', $nome, time() + 3600);
+  setcookie('email', $email, time() + 3600);
+  setcookie('cor', $cor, time() + 3600);
+} else {
+  setcookie('nome', $nome, time() + 3600);
+  setcookie('email', $email, time() + 3600);
+  setcookie('cor', $cor, time() + 3600);
+}
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Destino GET</title>
-
-    <link rel="stylesheet" href="dist/css/bootstrap.min.css">
-
-
-
-
+<html>
+  <head>
+    <title>Formulário de destino</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
-        body{
-            margin: 50px;
-            
-            <?php    
-            if ($cor) {
-                echo "background-color: $cor";
-            }        
-            ?>
+      body {
+        <?php 
+          
+        if ($cor) {
+          echo "background-color: $cor";
+        }  
 
-            
-        }
+        ?>
+      }
 
-        .container__colors{
-            display: flex;
-            justify-content: space-around;
-            flex-direction: row;
-        }
+      body{
+        margin: 40px;
+      }
 
-        .square__1{
-            margin-top: 10vh;
-            width: 300px;
-            height: 300px;
-            background-color: green; 
-            border: 2px solid black;
-        }
+      .quadrado {
+        height: 300px;
+        border: 1px solid #000;
+      }
 
-        .square__2{
-            margin-top: 10vh;
-            width: 300px;
-            height: 300px;
-            background-color: red;
-            border: 2px solid black;
-        }
-        .square__3{
-            margin-top: 10vh;
-            width: 300px;
-            height: 300px;
-            background-color: blue;
-            border: 2px solid black;
-        }
+      #red {
+        background-color: red;
+      }
+
+      #green {
+        background-color: green;
+      }
+
+      #blue {
+        background-color: blue;
+      }
     </style>
-</head>
-<body>
-    
-    <h1>Destino GET</h1>
-    <hr>
-
-
-    <?php 
-
+  </head>
+  <body>
+    <div class="container">
+      <h1>Destino GET</h1>
+      <hr/>
+      <?php 
+        
         if ($nome) {
-            echo "<h3>Nome: $nome</h3>";
-          }  
-    
-          if ($email) {
-            echo "<h3>Email: $email</h3>";
-          }
+            echo "<p>Nome: $nome</p>";
+        }  
 
-    ?>
+        if ($email) {
+            echo "<p>Email: $email</p>";
+        }
 
-    <a href="index.php?nome=Eder&email=epansani@gmail.com&cor=<?= $cor?>">
-        Enviar dados [nome = Eder | email = epansani@gmail.com]
-    </a>
-    <br><br>
-    <a href="index.php?nome=Dalva&email=dalvapede@gmail.com&cor=<?= $cor ?>">
-        Enviar dados [nome=Dalva | email = dalvapede@gmail.com]
-    </a>
-    <br><br>
-    <a href="index.php">
-        Limpar Dados
-    </a>
-    <br><br>
-    <a href="pagteste.php">
-        Faça o teste de salvamento
-    </a>
-
-
-    <div class="container__colors">
-        <a href="index.php?nome=<?= urlencode($nome)?>&email=<?= urlencode($email)?>&cor=green">
-            <div class="square__1">
-            </div>
+      ?>
+      <p>
+        <a href="index.php?nome=Eder&email=epansani@gmail.com&cor=<?= $cor ?>">[nome = Eder | email = epansani@gmail.com]</a>
+      </p>
+      <p>
+        <a href="index.php?nome=André&email=andrepinotti0@gmail.com&cor=<?= $cor ?>">[nome = André | email = andrepinotti0@gmail.com]</a>
+      </p>
+      <p>
+        <a href="index.php">Limpar tudo</a>
+      </p>
+      <div class="row" style="display: flex; justify-content: space-around">
+        <a href="index.php?nome=<?= $nome ?>&email=<?= $email ?>&cor=lightcoral" class="col-3">
+          <div id="red" class="quadrado"></div>
         </a>
-        <a href="index.php?nome=<?= urlencode($nome)?>&email=<?= urlencode($email)?>&cor=red">
-            <div class="square__2">
-            </div>
+        <a href="index.php?nome=<?= $nome ?>&email=<?= $email ?>&cor=lightgreen" class="col-3">
+          <div id="green" class="quadrado"></div>
         </a>
-        <a href="index.php?nome=<?= urlencode($nome)?>&email=<?= urlencode($email)?>&cor=blue">
-            <div class="square__3">      
-            </div>        
+        <a href="index.php?nome=<?= $nome ?>&email=<?= $email ?>&cor=lightblue" class="col-3">
+          <div id="blue" class="quadrado"></div>
         </a>
-
+      </div>
     </div>
-
-
-           
-
-
-
-</body>
+  </body>
 </html>
--- Aula7-28.03/Praticando3/index.php
+++ Aula7-28.03/Praticando3/index.php
@@ -1,15 +1,52 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulário</title>
-    
-    
+    <title>Formulário de cadastro</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <script>
+        // Função para criar/atualizar o cookie com o valor do campo
+        function updateCookie(fieldId, cookieName) {
+            var fieldValue = document.getElementById(fieldId).value;
+            document.cookie = cookieName + "=" + fieldValue + ";path=/";
+            console.log("Cookie " + cookieName + " criado/atualizado com o valor: " + fieldValue);
+        }
+
+        // Função para carregar os valores dos cookies e preencher os campos do formulário
+        function loadCookies() {
+            var cookies = document.cookie.split("; ");
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = cookies[i].split("=");
+                var field = document.getElementById(cookie[0]);
+                if (field) {
+                    field.value = cookie[1];
+                    console.log("Campo " + cookie[0] + " preenchido com o valor do cookie: " + cookie[1]);
+                }
+            }
+        }
+
+        // Event listener para os campos do formulário
+        document.addEventListener("DOMContentLoaded", function () {
+            loadCookies();
+
+            var formFields = document.querySelectorAll('input, select, textarea');
+            formFields.forEach(function (field) {
+                field.addEventListener("input", function () {
+                    updateCookie(field.id, field.id);
+                });
+            });
+        });
+    </script>
 
     <style>
         body{
-            margin: 30px;
+            margin: 40px;
         }
     </style>
 
@@ -17,44 +54,56 @@
 
 <body>
     <div class="container">
-        <h1>Formulário de cadastro</h1>
-        <hr/>
+
+        <h1 class="my-3">Formulário de cadastro</h1>
+        <hr>
+
         <div class="row">
-            <form>
-            <div class="mb-3">
-                <label for="nome" class="form-label">Nome completo:</label>
-                <input type="text" class="form-control" id="nome" name="nome" value="<?= $nome ?>">
+            <div class="col-8">
+                <form class="row g-3" action="#" method="POST">
+                    <div class="col-12">
+                        <div class="mb-2">
+                            <label for="nome">Nome completo:</label>
+                            <input class="form-control" type="text" name="nome" id="nome" required>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-2">
+                        <label for="tel">Telefone</label>
+                        <input class="form-control" type="tel" name="tel" id="tel" placeholder="(DD)XXXXX-XXXX"
+                            required>
+                    </div>
+                    <div class="col-md-8 mb-2">
+                        <label for="email">E-mail</label>
+                        <input class="form-control" type="email" name="email" id="email" placeholder="Digite seu email"
+                            required>
+                    </div>
+                    <div class="col-md-4 mb-2">
+                        <label for="date">CPF</label>
+                        <input class="form-control" type="text" name="cpf" id="cpf" placeholder="XXX.XXX.XXX-XX"
+                            required>
+                    </div>
+                    <div class="col-md-4 mb-2">
+                        <label for="date">Data de Nascimento</label>
+                        <input class="form-control" type="date" name="date" id="date" required>
+                    </div>
+                    <div class="col-md-4 mb-2">
+                        <label for="cep">CEP</label>
+                        <input class="form-control" type="text" name="cep" id="cep" placeholder="XX.XXX-XXX" required
+                            pattern="\d{2}\.\d{3}-\d{3}" title="Digite o CEP no formato XX.XXX-XXX">
+                    </div>
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-primary">Cadastrar</button>
+                        <button type="reset" class="btn btn-warning">Limpar</button>
+                    </div>
+                </form>
             </div>
-            <div class="row mb-3">
-                <div class="col-4">
-                <label for="telefone" class="form-label">Telefone:</label>
-                <input type="number" class="form-control" id="telefone" name="telefone" placeholder="(DD) XXXXX-XXXX" value="<?= $telefone ?>">
-                </div>
-                <div class="col-8">
-                <label for="email" class="form-label">E-mail:</label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="Digite seu email" value="<?= $email ?>">
-                </div>
-            </div>
-            <div class="row mb-3">
-                <div class="col-4">
-                <label for="cpf" class="form-label">CPF:</label>
-                <input type="number" class="form-control" id="cpf" name="cpf" placeholder="XXX.XXX.XXX-XX" value="<?= $cpf ?>">
-                </div>
-                <div class="col-4">
-                <label for="data" class="form-label">Data de nascimento:</label>
-                <input type="date" class="form-control" id="data" name="data" value="<?= $data ?>">
-                </div>
-                <div class="col-4">
-                <label for="cep" class="form-label">CEP:</label>
-                <input type="number" class="form-control" id="cep" name="cep" placeholder="XX.XXX-XXX" value="<?= $cep ?>">
-                </div>
-            </div>
-
-            <button type="submit" class="btn btn-primary">Cadastrar</button>
-            <button type="reset" class="btn btn-warning">Limpar</button>
-            </form> 
+            
         </div>
-    </div>
 
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
